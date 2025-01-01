fb61f86107ed5c874b4debdda6783877b45dda50
Muhammad Bin Khalid <83428194+MIbnEKhalid@users.noreply.github.com>
2025-01-01 09:09:21 +0500

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -2,13 +2,11 @@
 #include <vector>
 #include <string>
 #include <algorithm>
-#include <fstream>
-
-#ifdef _WIN32
-#include <windows.h>
-#else
-#include <unistd.h>
-#endif
+#include <fstream> 
+#include <windows.h>   
+#include <conio.h>
+#include <iomanip> 
+#include <sstream>   
 
 using namespace std;
 
@@ -23,6 +21,61 @@ const string BLUE = "\033[34m";
 const string MAGENTA = "\033[35m";
 const string CYAN = "\033[36m";
 const string WHITE = "\033[37m";
+
+void CentredMessage(const string& textMessage)
+{
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	int rows;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+	rows = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+
+	istringstream iss(textMessage);
+	string line;
+
+	while (getline(iss, line))
+	{
+		if (line.size() <= rows)
+		{
+			int startPosition = (rows - line.size()) / 2;
+			cout << setw(startPosition + line.size()) << line << endl;
+		}
+
+		else
+		{
+			// Split the line into two parts for better centering
+			size_t middle = line.size() / 2;
+			string part1 = line.substr(0, middle);
+			string part2 = line.substr(middle);
+			int startPositionPart1 = (rows - part1.size()) / 2;
+			int startPositionPart2 = (rows - part2.size()) / 2;
+			cout << setw(startPositionPart1 + part1.size()) << part1 << endl;
+			cout << setw(startPositionPart2 + part2.size()) << part2 << endl;
+		}
+	}
+}
+
+void rlines(const string& textMessage)
+{
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	int rows;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+	rows = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+
+	istringstream iss(textMessage);
+	string line;
+
+	if (textMessage.size() > 1)
+	{
+//Only One Character is allowed
+		return;
+	}
+
+	for (int i = 0; i < rows; i++)
+	{
+		cout << textMessage;
+	}
+	cout << "\n";
+} 
 
 // Function to clear the console
 void clearConsole()
@@ -39,6 +92,26 @@ void pauseConsole()
 {
 	cout << "Press Enter to continue...";
 	cin.ignore();
+}
+void displayMenu()
+{
+	clearConsole();
+	rlines("-");
+	cout << YELLOW;
+	CentredMessage("Developed by Muhammad Bin Khalid");
+	CentredMessage("Source Code: https://github.com/MIbnEKhalid/ICTLabProject");
+	cout << RESET;
+	rlines("-");
+
+	cout << CYAN;
+	CentredMessage("\nWelcome to the Spelling Practice Application!\n");
+	cout << RESET;
+
+	cout << "1. Start Spelling Practice\n";
+	cout << "2. Add a New Word\n";
+	cout << "3. View Spelling Practice Words\n";
+	cout << "4. Exit\n";
+	cout << "Please choose an option: ";
 }
 
 // Function to get user input
@@ -109,12 +182,35 @@ void spellingPractice(const vector<string>& words)
 		<< RESET;
 
 	cout << "How many words out of " << words.size() << " do you want to practice? ";
-	int numWords;
+	string numWords;
 	cin >> numWords;
 	cin.ignore(); // To consume the newline character left by cin
 
-	while (numWords > words.size() || numWords <= 0)
+
+
+	while (!isdigit(numWords[0]) || numWords.size() > 1)
 	{
+		if (numWords == BACK_TO_MENU)
+		{
+			return;
+			displayMenu();
+		}
+		cout << RED << "Invalid Input\n"
+			<< RESET;
+		cout << "Entre Again: ";
+		cin >> numWords;
+		cin.ignore(); // To consume the newline character left by cin
+
+	}
+	int num = stoi(numWords);
+
+	while (num > words.size() || num < 1)
+	{
+		if (numWords == BACK_TO_MENU)
+		{
+			return;
+			displayMenu();
+		}
 		cout << RED << "Invalid number of words.\n"
 			<< RESET;
 		cout << "Entre Again: ";
@@ -123,7 +219,7 @@ void spellingPractice(const vector<string>& words)
 
 	}
 
-	for (int i = 0; i < numWords; ++i)
+	for (int i = 0; i < num; ++i)
 	{
 		cout << "Spell this word: " << words[i] << "\n";
 		string userInput = getUserInput();
@@ -144,8 +240,7 @@ void spellingPractice(const vector<string>& words)
 			cout << RED << "Incorrect. The correct spelling is: " << words[i] << "\n\n"
 				<< RESET;
 		}
-	}
-
+	} 
 	cout << CYAN << "Spelling practice completed!\n"
 		<< RESET;
 	cout << "You spelled " << correctCount << " out of " << numWords << " words correctly.\n";
@@ -203,21 +298,7 @@ void viewWords(const vector<string>& words)
 	pauseConsole();
 }
 
-void displayMenu()
-{
-	clearConsole();
-	cout << "---------------------------------------\n"; 
-	cout << MAGENTA << "Developed by Muhammad Bin Khalid\n"
-		<< RESET;
-	cout << "----------------------------------------\n";
-	cout << BLUE << "Welcome to the Spelling Practice Application!\n"
-		<< RESET;
-	cout << "1. Start Spelling Practice\n";
-	cout << "2. Add a New Word\n";
-	cout << "3. View Spelling Practice Words\n";
-	cout << "4. Exit\n";
-	cout << "Please choose an option: ";
-}
+
 
 int main()
 {
@@ -263,3 +344,4 @@ int main()
 
 	return 0;
 }
+
